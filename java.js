ebc12cab7322dc21252bd75a365a01d8ffcbc8ec
erik-haggstrom <erik.haggstrom0@gmail.com>
2023-09-09 11:13:02 +0200

    Did the creation for the div grid

--- java.js
+++ java.js
@@ -1,9 +1,14 @@
 const container = document.querySelector('.container');
+const knapp = document.querySelector('.knapp')
 
 function createDiv(num) {
   for (let i = 0; i < num; i++){
     let box = document.createElement('div');
     box.classList.add('box')
+    box.addEventListener('mouseover', () => {
+      box.style.backgroundColor = 'blue'
+    });
     container.appendChild(box);
   }
 }
+
