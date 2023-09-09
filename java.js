85a0183ece2fba1e15148a81fd1212ae9df10458
erik-haggstrom <erik.haggstrom0@gmail.com>
2023-09-09 13:59:29 +0200

    Completed the sketchpad

--- java.js
+++ java.js
@@ -1,14 +1,36 @@
 const container = document.querySelector('.container');
-const knapp = document.querySelector('.knapp')
 
 function createDiv(num) {
-  for (let i = 0; i < num; i++){
+  for (let i = 0; i < num*num; i++){
     let box = document.createElement('div');
     box.classList.add('box')
     box.addEventListener('mouseover', () => {
-      box.style.backgroundColor = 'blue'
+    box.style.backgroundColor = 'blue'
     });
     container.appendChild(box);
+    let hojd = box.offsetHeight * num;
+    let bred = box.offsetWidth * num;
+    container.style.height = hojd + "px";
+    container.style.width = bred + "px";
   }
 }
 
+function removeGrid() {
+  let children = document.querySelector('.container').children;
+  let barn = Array.from(children);
+  barn.forEach((element) => {
+    element.remove();
+  });
+}
+
+function newGrid() {
+  let gridSize = prompt("What size should the new grid be?");
+  if (gridSize > 100){
+    alert('The max is 100*100 please enter a lower number')
+  }
+  else{
+    removeGrid();
+    createDiv(gridSize);
+  }
+  
+}
